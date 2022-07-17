9605a91f7ccd5fa65ba7ad01568a29f391ec2ccb
Rodrigo Vargas <rodrigo.vargas.abarca@est.una.ac.cr>
2022-07-16 20:38:10 -0600

    feat: add navbar collapsed styles, options and functions

--- boombit-room-page/src/components/PageNavbar.js
+++ boombit-room-page/src/components/PageNavbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import $ from 'jquery';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import $ from 'jquery';
 
 function PageNavbar() {
 
@@ -22,6 +24,9 @@ function PageNavbar() {
                 $("#navbar").removeClass("bg-light");
             }
         })
+        $(".navbar-toggler").click(function () {
+            console.log("abriendo");
+        })
     })
 
     return (
@@ -31,11 +36,28 @@ function PageNavbar() {
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto" />
-                    <Nav className="my-2 my-lg-0" style={{ maxHeight: '100vh' }} navbarScroll>
+                    <Nav className="my-2 my-lg-0 my-auto" style={{ maxHeight: '100vh' }} navbarScroll>
                         <Nav.Link href="#home">Inicio</Nav.Link>
                         <Nav.Link href="#blog">Blog</Nav.Link>
                         <Nav.Link href="#activities">Actividades</Nav.Link>
-                        <Button variant="outline-secondary">Contáctenos</Button>{' '}
+                        <Nav.Link href="#deputies" className='d-block d-sm-none'>Diputados</Nav.Link>
+                        <Nav.Link href="#principles" className='d-block d-sm-none'>Principios de nuestro partido</Nav.Link>
+                        <Nav.Link href="#affiliation" className='d-block d-sm-none'>Afiliación</Nav.Link>
+                        <Button variant="outline-secondary">Contáctenos</Button>
+                        <Container className='d-flex justify-content-around d-sm-none'>
+                            <Button className='btn btn-icon'>
+                                <FontAwesomeIcon icon={faFacebook} size="2x"/>
+                            </Button>
+                            <Button className='btn btn-icon'>
+                                <FontAwesomeIcon icon={faTwitter} size="2x"/>
+                            </Button>
+                            <Button className='btn btn-icon'>
+                                <FontAwesomeIcon icon={faInstagram} size="2x"/>
+                            </Button>
+                            <Button className='btn btn-icon'>
+                                <FontAwesomeIcon icon={faWhatsapp} size="2x"/>
+                            </Button>
+                        </Container>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
