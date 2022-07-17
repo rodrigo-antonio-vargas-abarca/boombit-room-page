bbd80bcce24b792195aae946456454796515fda2
Rodrigo Vargas Abarca <rvargasabarca@hotmail.com>
2022-07-17 16:32:18 -0600

    feat: include blog styles for desktop version

--- boombit-room-page/src/components/Blog.js
+++ boombit-room-page/src/components/Blog.js
@@ -8,32 +8,45 @@ import image3 from '../images/pages/blog/blog-3.png';
 function Blog() {
     return (
         <div className='section-blog'>
-            <h2 className='text-light font-weight-bold'>Blog</h2>
-            <section className='container'>
-                <Card className="mx-2">
+            <h2 className='text-light font-weight-bold pt-4'>Blog</h2>
+            <section className='container mt-4 mb-4'>
+                <Card className="mx-3">
                     <Card.Img variant="top" src={image1} />
                     <Card.Body>
-                        <Card.Title>CATEGOTY</Card.Title>
-                        <Card.Text>
-                            Cursus lectus blandit ornare turpis purus pharetra Id amet enim.
+                        <Card.Subtitle className="mb-2">CATEGOTY</Card.Subtitle>
+                        <Card.Title>Cursus lectus blandit ornare turpis purus pharetra Id amet enim.</Card.Title>
+                        <Card.Text className='d-none d-md-block'>
+                            Venenatis amet, nisi odio nisl. In id hac gravida faucibus eu suspendisse amet pellentesque. At mi pretium auctor faucibus adipiscing senectus pharetra...
                         </Card.Text>
                     </Card.Body>
                 </Card>
-                <Card className="mx-2">
+                <Card className="mx-3">
                     <Card.Img variant="top" src={image2} />
                     <Card.Body>
-                        <Card.Title>CATEGOTY</Card.Title>
-                        <Card.Text>
-                            Cursus lectus blandit ornare turpis purus pharetra Id amet enim.
+                        <Card.Subtitle className="mb-2">CATEGOTY</Card.Subtitle>
+                        <Card.Title>Cursus lectus blandit ornare turpis purus pharetra Id amet enim.</Card.Title>
+                        <Card.Text className='d-none d-md-block'>
+                            Venenatis amet, nisi odio nisl. In id hac gravida faucibus eu suspendisse amet pellentesque. At mi pretium auctor faucibus adipiscing senectus pharetra...
                         </Card.Text>
                     </Card.Body>
                 </Card>
-                <Card className="mx-2">
+                <Card className="mx-3">
                     <Card.Img variant="top" src={image3} />
                     <Card.Body>
-                        <Card.Title>CATEGOTY</Card.Title>
-                        <Card.Text>
-                            Cursus lectus blandit ornare turpis purus pharetra Id amet enim.
+                        <Card.Subtitle className="mb-2">CATEGOTY</Card.Subtitle>
+                        <Card.Title>Cursus lectus blandit ornare turpis purus pharetra Id amet enim.</Card.Title>
+                        <Card.Text className='d-none d-md-block'>
+                            Venenatis amet, nisi odio nisl. In id hac gravida faucibus eu suspendisse amet pellentesque. At mi pretium auctor faucibus adipiscing senectus pharetra...
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card className="mx-3">
+                    <Card.Img variant="top" src={image3} />
+                    <Card.Body>
+                        <Card.Subtitle className="mb-2">CATEGOTY</Card.Subtitle>
+                        <Card.Title>Cursus lectus blandit ornare turpis purus pharetra Id amet enim.</Card.Title>
+                        <Card.Text className='d-none d-md-block'>
+                            Venenatis amet, nisi odio nisl. In id hac gravida faucibus eu suspendisse amet pellentesque. At mi pretium auctor faucibus adipiscing senectus pharetra...
                         </Card.Text>
                     </Card.Body>
                 </Card>
