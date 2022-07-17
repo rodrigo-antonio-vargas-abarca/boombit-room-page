78793051bc834d62e449c62a0e8a4538be381462
Rodrigo Vargas <rodrigo.vargas.abarca@est.una.ac.cr>
2022-07-16 20:44:37 -0600

    feat: include navbar in app, include sass changes with navbar styles

--- boombit-room-page/src/App.js
+++ boombit-room-page/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/PageNavbar';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hola Mundo</h1> 
+      <Navbar />
     </div>
   );
 }
