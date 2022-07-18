dc9f24b69c24ff4625c126d0aae2d51fa35ccb22
Rodrigo Vargas Abarca <rvargasabarca@hotmail.com>
2022-07-17 23:57:36 -0600

    feat: import carousel styles and customizations, add styles for axes section

--- boombit-room-page/src/components/Blog.js
+++ boombit-room-page/src/components/Blog.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import image1 from '../images/pages/blog/blog-1.png';
 import image2 from '../images/pages/blog/blog-2.png';
